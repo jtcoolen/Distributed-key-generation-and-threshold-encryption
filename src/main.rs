5f7b228ed41e419d84e053285b0f691ac916965a
Julien Coolen <jtcoolen@pm.me>
2024-06-10 23:23:04 +0200

    wip proof of knowledge for discrete log

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,55 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>
 
-use class_group::{ABDeltaTriple, BinaryQF};
-use curv::{arithmetic::Converter, BigInt};
+use class_group::{primitives::cl_dl_lcm::HSMCL, ABDeltaTriple, BinaryQF};
+use curv::{
+    arithmetic::{Converter, Samplable},
+    BigInt,
+};
+use num_traits::One;
+
+#[derive(Clone, Debug)]
+struct NizkDlogProof {
+    public_coin: BigInt,
+    blinded_log: BigInt,
+}
+
+// Public coin (computed via Fiat-Shamir)
+fn nizk_dlog_challenge(base: &BinaryQF, h: &BinaryQF, a: &BinaryQF) -> BigInt {
+    let mut hasher = blake3::Hasher::new();
+    hasher.update(base.to_bytes().as_slice());
+    hasher.update(h.to_bytes().as_slice());
+    hasher.update(a.to_bytes().as_slice());
+    let hash = hasher.finalize();
+    BigInt::from_bytes(hash.as_bytes())
+}
+
+// Schnorr's sigma protocol for proof of discrete logarithm,
+// transformed into a non-interactive proof through Fiat-Shamir
+fn nizk_dlog_prove(base: &BinaryQF, h: &BinaryQF, log: &BigInt, bound: &BigInt) -> NizkDlogProof {
+    let r = BigInt::sample_range(&BigInt::one(), bound);
+    let a = base.exp(&r);
+    let c = nizk_dlog_challenge(base, h, &a);
+    let s = r + log * &c;
+    NizkDlogProof {
+        public_coin: c,
+        blinded_log: s,
+    }
+}
+
+fn nizk_dlog_verify(proof: NizkDlogProof, base: &BinaryQF, h: &BinaryQF, bound: &BigInt) -> bool {
+    /*if &proof.blinded_log >= &bound {
+        return false;
+    }*/
+    let b = h.exp(&proof.public_coin).inverse();
+    let a = base.exp(&proof.blinded_log).compose(&b).reduce();
+    let c = nizk_dlog_challenge(base, h, &a);
+    c.eq(&proof.public_coin)
+}
 
 fn main() {
     unsafe {
-        class_group::pari_init(10000000, 2);
+        class_group::pari_init(50000000, 2);
     }
     let a: BigInt = BigInt::from_str_radix("1347310664179468558147371727982960102805371574927252724399119343247182932538452304549609704350360058405827948976558722087559341859252338031258062288910984654814255199874816496621961922792890687089794760104660404141195904459619180668507135317125790028783030121033883873501532619563806411495141846196437", 10).unwrap();
 
@@ -29,5 +72,22 @@ fn main() {
     let pf = BinaryQF::binary_quadratic_form_disc(&abd);
     let pari_qf = pf.qf_to_pari_qf();
     let pf2 = BinaryQF::pari_qf_to_qf(pari_qf);
-    assert_eq!(pf, pf2)
+    assert_eq!(pf, pf2);
+
+    let q = BigInt::from_str_radix(
+        "115792089210356248762697446949407573529996955224135760342422259061068512044369",
+        10,
+    )
+    .unwrap();
+    let hsmcl = HSMCL::keygen(&q, &1600);
+    let u = hsmcl.pk.gq;
+    let group = u;
+
+    let bound = BigInt::from_str_radix("134731066417946855817371727982960102805371574927252724399119343247182932538452304549609704350360058405827948976558722087559341859252338031258062288910984654814255199874816496621961922792890687089794760104660404141195904459619180668507135317125790028783030121033883873501532619563806411495141846196437", 10).unwrap();
+    let log = BigInt::sample_range(&BigInt::from_bytes(&[1u8]), &bound);
+
+    let base = group.clone();
+    let h = base.exp(&log);
+    let proof = nizk_dlog_prove(&base, &h, &log, &bound);
+    assert!(nizk_dlog_verify(proof, &base, &h, &bound))
 }
